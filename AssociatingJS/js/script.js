5b979468eed0578f33eb429e78002d25c1858254
istacy <stacy@Stacys-MacBook-Pro.local>
2013-11-01 10:57:40 -0500

    adding a var to display name

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -2,5 +2,9 @@
 
 var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
 
-muppetNames [1] = "Rizzo";
-console.log(muppetNames[1]);
+muppetNames[1] = "Rizzo"; //change index #1 to Rizzo
+muppetNames[2] = "Fozzy";
+
+var num = 2;
+
+console.log(muppetNames[num]);
