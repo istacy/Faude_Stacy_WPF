6837291a8e2b74a337d84bb6b7c9139f0d9ce120
istacy <stacy@Stacys-MacBook-Pro.local>
2013-11-01 11:19:52 -0500

    checking even or odd numbers with remainder

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,6 +1,6 @@
 // Expressions - Modulo Operator
 
-var remainder = 32 % 10;
+var remainder = 32 % 2; //Checking remainder is a good way to tell if a number is even or odd
 console.log(remainder);
 
 
