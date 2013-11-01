2292fec6c4944739f73ca0a211b74e00dbc5d587
istacy <stacy@Stacys-MacBook-Pro.local>
2013-11-01 11:57:30 -0500

    Expressions - Assignment Operators
    using the a-- operator

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -3,10 +3,8 @@
 
 var a = 3;
 
-//a = a + 4; //always make sure storage container is on left
+a --;  // same as a = a - 1 // a -= 1
 
-a += 4;  //+= is an assignment operator
 
-//a = a / 4
 
 console.log(a);
