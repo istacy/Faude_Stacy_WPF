66bb24dadae31dea4ddd17b6d1bfc4728a665580
istacy <stacy@Stacys-MacBook-Pro.local>
2013-11-01 11:25:43 -0500

    // Expressions - PEMDAS Order of Operations

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,7 +1,14 @@
-// Expressions - Modulo Operator
+// Expressions - PEMDAS Order of Operations
 
-var remainder = 32 % 2; //Checking remainder is a good way to tell if a number is even or odd
-console.log(remainder);
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
 
+//the average is the sum of the items divided by the number of items
+
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+
+console.log(average);
 
 
