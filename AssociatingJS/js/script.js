3322a773dbda71096d9cee973be8202a10cde5b7
istacy <stacy@Stacys-MacBook-Pro.local>
2013-11-03 09:29:07 -0600

    Expressions Worksheet
    Calcualting the area of a Rectangle

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,10 +1,2 @@
-// Basic Expressions
-// Calculating with & Changing the values and variables
-// adding three to the variable
+// Activity Expression Worksheet
 
-var a = 2;
-var b;
-
-b = a + 3;
-
-console.log(a);
---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,15 @@
+// Stacy Faude
+// Date: 11-3-13
+// Assignment:  Expressions Worksheet
+
+
+//Calculate the area of a rectangle given the width and height
+
+//The area of a rectangle is the width * height
+
+var width = 4;
+var height = 5;
+
+var area = width * height;
+
+console.log(area);
