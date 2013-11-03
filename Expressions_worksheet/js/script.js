f90448791210dd6baa614ef40304911e13de0454
istacy <stacy@Stacys-MacBook-Pro.local>
2013-11-03 12:51:52 -0600

    Expressions Worksheet - Discounts
    Calculating the percent off an item with and without sales tax

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -3,16 +3,40 @@
 // Assignment:  Expressions Worksheet
 
 
-//Average Shopping Bill
-
-//Array with 5 weekly grocery totals
-//(120.25, 45.45, 76.54, 51.99, 23.88)
-
-var grocerys = [120.25, 45.45, 76.54, 51.99, 23.88];
-
-var groceryTotal = grocerys[0] + grocerys[1] + grocerys[2] + grocerys[3] + grocerys[4];
-
-var groceryAverage = (grocerys[0] + grocerys[1] + grocerys[2] + grocerys[3] + grocerys[4]) / 5;
+//Discounts
+var originalPrice = 49.99;
+var discount = 20;
+var itemDesc = "video game";
+var salesTax = 5.5;
 
 
-console.log("You have spent a total of $" + groceryTotal + " on groceries over 5 weeks. That is an average of $" + groceryAverage + " per week.")
+//Price of item with tax
+
+//First find the amount the discount will be
+//49.99 * 20% = 9.998
+discountAmout = (originalPrice * discount / 100);
+
+//Next Find the Price with the discount
+// 49.99 - 9.998 = 39.992
+discountPrice = (originalPrice - discountAmout);
+
+//Find the amount the tax will be
+//39.992 * 5.5% = 2.19956
+taxAmoumt = (discountPrice * salesTax / 100);
+
+//Find the discount price with tax
+//39.992 + 2.19956
+discountPriceWithTax = discountPrice + taxAmoumt
+
+//Price of item without tax
+
+//First find the amount the discount will be
+//49.99 * 20% = 9.998
+discountAmout = (originalPrice * discount / 100);
+
+//Next Find the Price with the discount
+// 49.99 - 9.998 = 39.992
+discountPrice = (originalPrice - discountAmout);
+
+
+console.log("Your " + itemDesc + " was originally $" + originalPrice + " , but after a " + discount + "% is now $" + discountPrice + " without tax, and $" + discountPriceWithTax + " with tax");
