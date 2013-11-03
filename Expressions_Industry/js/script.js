72ea20087d55c494ea47a9438f084ad832e2fb70
istacy <stacy@Stacys-MacBook-Pro.local>
2013-11-03 14:29:58 -0600

    Expressions - Industry
    Added Comments to the code

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -6,20 +6,29 @@
 
 alert("This will estimate the cost of a new website design");
 
+//Set Costs
 var pageCost = 100;
 var imageDigitalCost = 5;
 var imagePhotoCost = 10;
 var animationCost = 50;
 var scriptCost = 25;
 
+//Number of Pages
 var noPages = prompt("Please enter the number of pages required? \n (Home, About us, Catalog, Contact, etc.");
+//Number of Digital Images
 var imagesDigital = prompt("How many digital images will you supply?");
+//Number of Photographs
 var imagesPhotos = prompt("How many images will be photographs");
 
+//alert of prices for images
 alert("We charge $5 per digital image and \n $10 per photograph");
 
+//Number of Animations
 var animation = prompt("How many animations will you need?");
+//Number of Scripts
 var scripts = prompt("About how many interactive Graphic Scripts? \n (Image Maps, Rollover Effects, Slide Shows, etc.");
+
+//Calculating total cost of the website
 
 var total = (noPages * pageCost) + (imagesDigital * imageDigitalCost) + (imagesPhotos * imagePhotoCost) + (animation * animationCost) + (scripts * scriptCost);
 var total = (noPages * pageCost) + (imagesDigital * imageDigitalCost) + (imagesPhotos * imagePhotoCost) + (animation * animationCost) + (scripts * scriptCost);
