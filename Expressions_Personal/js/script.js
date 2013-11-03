9d1b6a121d0766e2fa38ba70319022007b3bae39
istacy <stacy@Stacys-MacBook-Pro.local>
2013-11-03 14:43:50 -0600

    Expressions - Wacky

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -7,6 +7,7 @@ alert("This will calculate your weekly gross pay from \n Marathon Cheese and Vir
 
 //Enter in hourly wage and hours worked for Marathon Cheese
 
+var job1 = "Marathon Cheese"
 var wageJob1 = prompt("Please enter your hourly wage for Marathon Cheese.");
 var HoursJob1 = prompt("Please enter the number of hours you worked at Marathon Cheese.");
 var overTimeJob1 = prompt("How many hours of overtime did you work?");
@@ -16,6 +17,7 @@ overTimeJob1 = wageJob1 * 1.5;
 //Enter in projects finished at Virtual Vision
 //Pay for Virtual Vision is $250 per project.
 
+var job2 = "Virtual Vision"
 var projectsJob2 = prompt("Please enter how many projects you finished this week.");
 var wageJob2 = 250;
 
@@ -23,4 +25,4 @@ var grossPay = (wageJob1 * HoursJob1) + overTimeJob1 + (projectsJob2 * wageJob2)
 
 console.log(grossPay); //prints it out to console
 
-alert("Your gross pay is $" + grossPay);
+alert("Your gross pay for " + job1 + " and" + job2 + "is:  $" + grossPay);
---
+++ Expressions_Wacky/js/script.js
@@ -0,0 +1,28 @@
+// Stacy Faude
+// Date: 11-3-13
+// Assignment:  Expressions Personal
+
+//What am I calculating?
+//How much fruit do I have left
+
+//Enter the number of oranges you have
+
+var orangeBasket1 = prompt("Please enter the number of oranges in basket 1");
+var orangeBasket2 = prompt("Please enter the number of oranges in basket 2");
+var orangeBasket3 = prompt("Please enter the number of oranges in basket 3");
+
+//Enter the number of apples you have
+
+var applesBasket1 = prompt("Please enter the number of apples in basket 1");
+var applesBasket2 = prompt("Please enter the number of apples in basket 2");
+var applesBasket3 = prompt("Please enter the number of apples in basket 3");
+
+//Enter the number of pears you have
+
+var pearsBasket1 = prompt("Please enter the number of pears in basket 1");
+var pearsBasket2 = prompt("Please enter the number of pears in basket 2");
+var pearsBasket3 = prompt("Please enter the number of pears in basket 3");
+
+//
+
+var total = orangeBasket1
