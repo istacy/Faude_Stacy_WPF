755a7cbfc0119d0ec16cfe3f7b2cb68f822e35a3
istacy <stacy@Stacys-MacBook-Pro.local>
2013-11-04 11:46:32 -0600

    Expressions - Wacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -13,6 +13,11 @@ var startingMiles = prompt("Please enter your cars current mileage.");
 
 var gasPrice = prompt("Please enter the price per gallon.");
 
+var gasUsed = prompt("Please enter how many gallons of gas you purchased.");
+
+var endMiles = prompt("Please enter your ending milage.");
+
+//var pickingUp = prompt("What friend are you picking up? \n  Please select 1-5" + myFriends[0]);
 var myFriends = new Array();
     myFriends["Matthew"]=21;
     myFriends["Tyler"]=55;
@@ -20,15 +25,14 @@ var myFriends = new Array();
     myFriends["Liz"]=653;
     myFriends["Kira"]=478;
 
-var gasUsed = prompt("Please enter how many gallons of gas you purchased.");
-
-var endMiles = prompt("Please enter your ending milage.")
-
-
 //mpg = (endMiles - startingMiles) / gasUsed
 
-//var pickingUp = prompt("What friend are you picking up? \n  Please select 1-5" + myFriends[0]);
+var milesDriven = endMiles - startingMiles;
 
-var pricePerGallon = (((endMiles - startingMiles) / gasUsed)) / gasPrice)
+var milesPerGallon = milesDriven / gasUsed;
 
-console.log(pricePerGallon)
+var pricePerGallon = milesDriven / gasPrice;
+
+var info = prompt("You have driven" + milesDriven + " and get about" + milesPerGallon + "spending $" + pricePerGallon + "per mile");
+
+console.log(mpg);
