34e7a1749d773c9a8b65a952dd15d951d00cca1d
istacy <stacy@Stacys-MacBook-Pro.local>
2013-11-04 08:03:55 -0600

    Expressions - Wacky
    Added Array of Friends and miles

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -10,5 +10,11 @@
 var firstName = prompt("Please enter your first name.");
 var lastName = prompt("Please enter your last name.");
 var city = prompt("Please enter the city you live in.");
-var myFriends = ("Matthew", "Tyler", "Stephanie", "Liz", "Kira");
+var myFriends = new Array();
+    myFriends["Matthew"]=21;
+    myFriends["Tyler"]=55;
+    myFriends["Stephanie"]=157;
+    myFriends["Liz"]=653;
+    myFriends["Kira"]=478;
+
 
