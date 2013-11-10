783677ac29153ac4fdcc3bb48258530ccdf71337
Stacy Faude <istacy@fullsail.edu>
2013-11-10 17:12:16 -0600

    //Conditional Logic - Ternary Operators
    Age for a Book

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -5,9 +5,6 @@ var book;
 
 //If hte child is under 10, they get Green Eggs and Ham, otherwise they get the Time Machine
 
-if(age < 10){
-    book = "Green Eggs and Ham";
-}else{
-    book = "The Time Machine";
-}
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+
 console.log(book);
