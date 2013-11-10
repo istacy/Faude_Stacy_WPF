d70263f7ab69d77af9ce5997ecc18be526af88e8
Stacy Faude <istacy@fullsail.edu>
2013-11-10 11:54:46 -0600

    //Conditional Logic - Logical Operators

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -5,16 +5,12 @@ var iPhonePrice = 199.99;
 var payCheck = 200;
 
 //if the price of the phone is less than our budget AND if our paycheck is over 300
-if(iPhonePrice < budget){
+if(iPhonePrice < budget && paycheck > 300){
     console.log("We can buy the phone!");
 }else {
     console.log("No phone for you!!");
 }
-if(payCheck > 300){
-    console.log("We can buy the phone!!");
-}else{
-    console.log("No phone for you!!")
-}
+
 
 
 //Relational Operators
