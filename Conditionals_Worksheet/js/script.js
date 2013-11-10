81c4b91449f2161b32ddfa6861a9926d8145c39b
Stacy Faude <istacy@fullsail.edu>
2013-11-10 17:54:24 -0600

    Conditional Worksheet
    Stuff Your Face I

---
+++ Conditionals_Worksheet/js/script.js
@@ -0,0 +1,15 @@
+// Stacy Faude
+// Date: 11-10-13
+// Conditionals Worksheet
+
+//Do you weigh enough
+//You must be 250 or more to enter the heavyweight division of the Strawberry Festivals pie eatting contest
+
+var weight = 140;
+
+if(weight >= 250){
+    //if great than or equal to you qualify
+    console.log("The competitor qualifies for the heavyweight division!");
+}else{
+    console.log("The competitor needs to gain some wieght")
+}
