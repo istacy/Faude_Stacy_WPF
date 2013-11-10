b089cc51562106efc1aa8e9535c2af5678327ef9
Stacy Faude <istacy@fullsail.edu>
2013-11-10 11:07:36 -0600

    Conditional Logic - With an Expressions

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -12,6 +12,6 @@ if(kidHeight  > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
 }
-if(kidHeight < minHeight){
+if(kidHeight <= minHeight){
     console.log("Sorry Kid, you've got some growing to do!")
 }
