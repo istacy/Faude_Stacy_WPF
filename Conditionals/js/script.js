5d34f754f08790cbd7bdd727410eff81666adb69
Stacy Faude <istacy@fullsail.edu>
2013-11-10 16:49:21 -0600

    //Conditional Logic - Logical Operators

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -5,7 +5,7 @@ var iPhonePrice = 199.99;
 var wonLottery = true;
 
 //if the price of the phone is less than our budget AND if our paycheck is over 300
-if(iPhonePrice < budget || wonLottery === true){
+if(iPhonePrice < budget || wonLottery){
     console.log("We can buy the phone!");
 }else {
     console.log("No phone for you!!");
@@ -26,11 +26,9 @@ if(iPhonePrice < budget || wonLottery === true){
 // ^Exclusive or (XOR)
 // ! not
 
-
 //Logical Operators
 // && and
 // || or
-
 
 //6 number == "6" string computer will say its true
 //6 number === "6" string computer will say its false
