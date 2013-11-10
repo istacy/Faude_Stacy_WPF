e9844c94260367104ef19f8bd98f1b9a32625b34
Stacy Faude <istacy@fullsail.edu>
2013-11-10 10:54:48 -0600

    Conditionals

---
+++ Conditionals/js/script.js
@@ -0,0 +1,9 @@
+//Conditional Logic
+
+var oldEnough = true;
+
+//if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+    //code preformed if condition is true
+    console.log("You can ride the coaster!")
+}
