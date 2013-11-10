92bad3cf0c54989b7e48d3e3e7b61e52b4fc01b4
Stacy Faude <istacy@fullsail.edu>
2013-11-10 16:45:56 -0600

    Conditional Logic - Logical Operators

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -5,7 +5,7 @@ var iPhonePrice = 199.99;
 var payCheck = 200;
 
 //if the price of the phone is less than our budget AND if our paycheck is over 300
-if(iPhonePrice < budget && payCheck > 300){
+if(iPhonePrice < budget && payCheck < 300){
     console.log("We can buy the phone!");
 }else {
     console.log("No phone for you!!");
