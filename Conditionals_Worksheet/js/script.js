1401a7d49083b2e2a224beb156c17c87b52d0e1a
Stacy Faude <istacy@fullsail.edu>
2013-11-11 11:31:36 -0600

    Conditionals Worksheet - Grade Letter Calculator
    Working!!!

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -24,7 +24,7 @@ var unit = prompt("Please enter a f for Fahrenheit or a c for Celsius");
 
 var calcCelsius = ((degree - 32) / 1.8000);
 
-if (unit = f){
+if(unit = f){
     console.log("The temperature is " + degree + "degrees");
 }else{
     console.log("The temperature is " + calcCelsius + " degrees");
@@ -40,56 +40,42 @@ if (unit = f){
 //Full Sails Graduate Grade Scale
 
 //Given: grade - in percent
-/*
+
 var grade = 95;
 
 
 
 if(grade < 69) {
-    finalGrade = (grade + " (F)");
+    finalGrade = (" (F)");
 }
 if(grade >= 70 && grade < 72) {
-    finalGrade = (grade + " (D");
+    finalGrade = (" (D");
 }
 if(grade >= 73 && grade < 79 ){
-    finalGrade = (grade + " (C)");
+    finalGrade = (" (C)");
 }
 if(grade >= 80 && grade < 84){
-    finalGrade = (grade + " (B)");
+    finalGrade = (" (B)");
 }
 if(grade >= 85 && grade < 89){
-    finalGrade = (grade + " (B+)");
+    finalGrade = (" (B+)");
 }
 if(grade >= 90 && grade < 94){
-    finalGrade = (grade + " (A)");
+    finalGrade = (" (A)");
 }
 if(grade >= 95){
-    finalGrade = (grade + " (A+)");
+    finalGrade = (" (A+)");
 }
 
 console.log("You have a " + grade + " %, which means you have earned a(n)" + finalGrade + " in the class!");
-*/
-//or
 
-/*var grade = 90;
-
-function grade() {
-    score = document.form1.grade.value;
-    if (score==100) {alert("Perfect score!");}
-    else if (score>=90) {alert("You got an A");}
-    else if (score>=80) {alert("You got a B");}
-    else if (score>=70) {alert("You got a C");}
-    else if (score>=60) {alert("You got a D");}
-    else {alert("Failure.");}
-}}
-*/
 
 //Group 3
 //Movie Ticket Price
 //The local movie theater in town has a ticket price of $12.00 but if you are a senior (55 and older), under 10 you get the discounted price of $7.00. In addition, if a customer is seeing a movie between 3pm and 5pm they can also get the discounted price.
 //Determine which of the two prices the customer is eligible for.
 
-var timeOfMovie = 3;
-var age = 28;
+//var timeOfMovie = 3;
+//var age = 28;
 
 
