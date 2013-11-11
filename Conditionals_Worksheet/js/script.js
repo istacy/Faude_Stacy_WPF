e3f78a92c3b8bc6dad9d678b33a28c2a20b0cce2
Stacy Faude <istacy@fullsail.edu>
2013-11-10 19:37:48 -0600

    Expressions Worksheet
    Celsius to Fahrenheit converter
    This code works for Fahrenheit Only right now...

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -5,11 +5,22 @@
 //Do you weigh enough
 //You must be 250 or more to enter the heavyweight division of the Strawberry Festivals pie eatting contest
 
-var weight = 140;
+/*var weight = 140;
 
 if(weight >= 250){
     //if great than or equal to you qualify
     console.log("The competitor qualifies for the heavyweight division!");
 }else{
     console.log("The competitor needs to gain some wieght")
-}
+}*/
+
+//Celsius to Fahrenheit Converter
+//Celsius = ((F-32)/1.8000)
+
+var degree = 90;
+var unit = prompt("Please enter a f for Fahrenheit or a c for Celcius");
+
+var calcCelcius = ((degree - 32) / 1.8000);
+
+
+console.log("The temperature is "+ calcCelcius + " Celcius. or The temperature is " + degree + " Fahrenheit.");
