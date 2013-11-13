2779a2c9b06d20a277f5d1db54a85091b15846d2
Stacy Faude <istacy@fullsail.edu>
2013-11-13 11:28:00 -0600

    Conditionals - Personal Calculation
    Cannot get this to work?

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -6,28 +6,31 @@ var apples = 5
 var bananas = 6
 var oranges = 3
 
-alert("There are currently " + apples + " apples, " + bananas + " bananas, " + oranges + " oranges");
+function eatApples(){
 
-var eat = prompt("How many apples would you like to eat?", "1");
+    alert("There are currently " + apples + " apples, " + bananas + " bananas, " + oranges + " oranges.");
 
-var eaten  = parseInt(eat);
-if(isNaN(eaten)){
-    console.log("You must enter a valid number of apples!");
-    eatApples();
-} else if(eaten > apples){
-    console.log("Sorry, but there are only " + apples + " apples.  You can not eat " + eaten + "apples!");
-    eatApples();
-} else if(eaten < 0){
-    console.log("Sorry, but you can not eat less than 0 apples!");
-    eatApples();
-} else {
-    apples -= eaten;
-    alert("Now there are only " + apples + " apples!");
-    if(apples > 0){
-        if(confirm("Would you like to eat more apples?")){
-            eatApples();
-        }
+    var eat    = prompt("How many apples would you like to eat?", "1");
+    var eaten  = parseInt(eat);
+
+    if(isNaN(eaten)){
+        alert("You must enter a valid number of apples!");
+        eatApples();
+    } else if(eaten > apples){
+        alert("Sorry, but there are only " + apples + " apples.  You can not eat " + eaten + " apples!");
+        eatApples();
+    } else if(eaten < 0){
+        alert("Sorry, but you can not eat less than 0 apples!");
+        eatApples();
     } else {
-        console.log("All of the apples are now gone!");
+        apples -= eaten;
+        alert("Now there are only " + apples + " apples!");
+        if(apples > 0){
+            if(confirm("Would you like to eat more apples?"))
+                eatApples();
+            }
+     {
+            alert("All of the apples are now gone!");
+        }
     }
 }
