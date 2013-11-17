d12c507ce3b2730141ba75403fc3af068aa2fd5d
Stacy Faude <istacy@fullsail.edu>
2013-11-16 20:23:30 -0600

    Conditionals - Wacky

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -3,14 +3,51 @@
 //Conditionals - Wacky
 
 
-var age = 21;
-var maxAge = 100;
-var minAge = 18;
+var car = "Ford";
 
-if(age > minAge){
-    if(age <= maxAge){
-        console.log("You meet the requirements");
-    } else{
-        console.log("You are too old");
-    }
+switch(car){
+    case "Ford":
+        console.log("Built Ford Tough");
+        break;
+    case "Chevy":
+        console.log("Baseball, hot dogs, apple pie and Chevrolet");
+        break;
+    case "Chevorlet":
+        console.log("Baseball, hot dogs, apple pie and Chevrolet");
+        break;
+    case "Volkswagen":
+        console.log("Drivers Wanted");
+        break;
+    case "Honda":
+        console.log("The Power of Dreams");
+        break;
+    case "Audi":
+        console.log("Never Follow");
+        break;
+    case "Porsche":
+        console.log("Porsche, There is no substitute!");
+        break;
+    case "Mercedes":
+        console.log("Engineered to move the human spirit.");
+        break;
+    case "Jaguar":
+        console.log("Unleash a Jaguar");
+        break;
+    case "BMW":
+        console.log("Sheer driving pleasure.");
+        break;
+    case "Jeep":
+        console.log("The toughest 4 letter word on wheels");
+        break;
+    case "Acura":
+        console.log("The road will never be the same");
+        break;
+    case "Audi":
+        console.log("Never Follow");
+        break;
+    case "Chrysler":
+        console.log("Inspiration comes standard");
+        break;
+    default:
+        console.log("I dont know that car?");
 }
