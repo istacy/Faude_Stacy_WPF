24d90accb9866575d48a759ab960c8ae2daa4de7
Stacy Faude <istacy@fullsail.edu>
2013-11-17 14:13:38 -0600

    Do While Loops

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -10,3 +10,14 @@ while (b > 0){
     console.log(b + 'kegs on the wall');
     b--; // increments or decrements the index
 }
+
+console.log('----- Do While Loops ------');
+
+
+var c = 10;
+
+do{
+    console.log(c + ' kegs on the wall');
+    c--;
+}
+while(c > 0);
