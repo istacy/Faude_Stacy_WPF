2d65420b81a60b4570e5a66c30e8d86ecec9983a
Stacy Faude <istacy@fullsail.edu>
2013-11-16 21:15:32 -0600

    Conditionals - Industry Calculator
    Converting Hex to RGB

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -3,14 +3,26 @@
 //Conditionals - Industry Calculation
 
 
-var age = 21;
-var maxAge = 100;
-var minAge = 18;
+//Hex color
+var hex = "#fA0";
+//Convert to RGB
+var rgb = hex2rgb(hex);
 
-if(age > minAge){
-    if(age <= maxAge){
-        console.log("You meet the requirements");
-    } else{
-        console.log("You are too old");
+
+// Calculation for converting hex to RGB: http://gristle.tripod.com/hexconv.html
+function hex2rgb(hex) {
+    if (hex[0]=="#") hex=hex.substr(1);
+    if (hex.length==3) {
+        var temp=hex; hex='';
+        temp = /^([a-f0-9])([a-f0-9])([a-f0-9])$/i.exec(temp).slice(1);
+        for (var i=0;i<3;i++) hex+=temp[i]+temp[i];
+    }
+    var triplets = /^([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})$/i.exec(hex).slice(1);
+    return {
+        red:   parseInt(triplets[0],16),
+        green: parseInt(triplets[1],16),
+        blue:  parseInt(triplets[2],16)
     }
 }
+
+console.log(" "+hex+" \u2192 rgb("+rgb.red+","+rgb.green+","+rgb.blue+") ");
