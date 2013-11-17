391db35eacdfc483a30ad221c4a267c7b5c77db8
Stacy Faude <istacy@fullsail.edu>
2013-11-17 17:33:17 -0600

    Functions Worksheet
    Example Problem

---
+++ Functions_Worksheet/js/script.js
@@ -0,0 +1,27 @@
+//Stacy Faude
+//Functions Worksheet
+//Nov 17, 2013
+
+//Example Calculate the area of a rectangle
+console.log("----- Example Problem -----");
+
+calcArea(5, 6);
+
+function calcArea(w, h){
+    var area = w * h;
+    console.log("The Area of Rectangle is " + area);
+}
+
+//Calculate the Circumference of a circle
+
+console.log("----- Circumference of a circle -----");
+
+//circumference of the circle = 2 * radius * PI
+var rad = calcCirc(5)
+
+function calcCirc(radius){
+    var circ = 2 * radius * 3.1415;
+    return circ;
+}
+
+console.log("The circumference of the circle is " + rad );
