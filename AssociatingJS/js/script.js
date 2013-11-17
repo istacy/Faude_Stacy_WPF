3b243fe3c30fc948d11fa3b720e3895212aa3425
Stacy Faude <istacy@fullsail.edu>
2013-11-17 16:01:51 -0600

    Functions - Variable Scope

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -20,10 +20,10 @@
 }*/
 
 //Parenthesis are for parameters
-//functions have hames so we can refer to them later.
+//functions have names so we can refer to them later.
 
 
-
+//Functions - Function
 function outputMsg(){
     console.log("Hello world");
 }
@@ -34,3 +34,31 @@ function calcArea(){
     var area = width * height;
     console.log(area);
 }
+
+//Function - Function Execution
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
+calcArea();
+
+//Function - Variable Scope
+//always prefer to use the vairable within its own function
+
+var width = 5; //scoped outside of function
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    //console.log(area);
+}
+console.log(width); //this will pull the var from outside the function
+calcArea();
+
+//Functions - Arguments and Parameters
+//Getting stuff into a function
+
